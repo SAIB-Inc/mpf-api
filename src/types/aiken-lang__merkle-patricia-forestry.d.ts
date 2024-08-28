1452625067e168bcd913c1a603c11287b399ac5e
Clark Alesna <clark_alesna@hotmail.com>
2024-08-29 01:49:52 +0800

    chore: add new type def for MPF library august 29, 2024 (#1)

--- src/types/aiken-lang__merkle-patricia-forestry.d.ts
+++ src/types/aiken-lang__merkle-patricia-forestry.d.ts
@@ -5,6 +5,7 @@ declare module '@aiken-lang/merkle-patricia-forestry' {
         async insert: (key: Buffer | string, value: Buffer | string) => Promise<Trie>;
         async delete: (key: Buffer | string) => Promise<Trie>;
         async prove: (key: Buffer | string) => Promise<Proof>;
+        async fetchChildren(depth: number): Promise<void>;
     }
     export class Proof {
         constructor();
